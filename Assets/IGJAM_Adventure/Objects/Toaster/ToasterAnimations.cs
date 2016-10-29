f33d4f61c80a53a7bce87f6574b347208d7f6ccb
Lars Engel <lars.engel@innogames.com>
2016-10-29 17:33:05 +0200

    Working on Story and Animations

---
+++ Assets/IGJAM_Adventure/Objects/Toaster/ToasterAnimations.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine.Unity;
+
+public class ToasterAnimations : MonoBehaviour {
+
+    void SetBurning() {
+        gameObject.GetComponent<SkeletonAnimation>().AnimationName = "toaster_burning";
+        gameObject.GetComponent<SkeletonAnimation>().loop = true;
+
+    }
+
+    void SetBurnt() {
+        gameObject.GetComponent<SkeletonAnimation>().AnimationName = "toaster_burnt";
+        gameObject.GetComponent<SkeletonAnimation>().loop = true;
+
+    }
+
+    void SetSwitch() {
+        gameObject.GetComponent<SkeletonAnimation>().loop = false;
+        gameObject.GetComponent<SkeletonAnimation>().AnimationName = "toaster_burnt_switch";
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
