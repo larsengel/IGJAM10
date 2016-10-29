8b1b6199308d6e0bfc6cae3b3d08475a6a4f8ed5
Lars Engel <lars.engel@innogames.com>
2016-10-29 23:57:43 +0200

    Hand

---
+++ Assets/IGJAM_Adventure/Objects/Hand/hand_movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hand_movement : MonoBehaviour {
+    public GameObject _start;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+        transform.position = new Vector3(pos_move.x, pos_move.y, pos_move.z);
+
+        LineRenderer lr = GetComponent<LineRenderer>();
+        Vector3[] pos = new Vector3[2];
+        pos[0] = transform.position;
+        pos[1] = _start.transform.position;
+        lr.SetPositions(pos);
+    }
+}
